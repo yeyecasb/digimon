40646bef8dffb7e4bc38d5f9df04e9fc1fd4bfdd
Claudio Santibanez <yeyecasb@hotmail.com>
2023-04-15 13:00:15 -0400

    Luchando con el buscar

--- Assets/js/logica.js
+++ Assets/js/logica.js
@@ -1,58 +1,52 @@
-$(document).ready(function(){
-    let contenido=document.getElementById("listado");
+/*let contenido = document.getElementById("listado");
 
 fetch('https://digimon-api.vercel.app/api/digimon')
-.then(response=>response.json())
-.then(resp=>{crearListado(resp)});
+    .then(response => response.json())
+    .then(resp => { crearListado(resp) });
 
-function crearListado(resp){
+function crearListado(resp) {
 
-    for(let datoTemporal of resp){
+    for (let datoTemporal of resp) {
         contenido.innerHTML +=
-        `
+            `
         <tr><td><img src=${datoTemporal.img} height="40" weight="40" id="imagen"></td>
-        <td>${datoTemporal.name}</td>
-        <td>${datoTemporal.level}</td></tr>
+        <td><strong>${datoTemporal.name}</strong></td>
+        <td><strong>${datoTemporal.level}</strong></td></tr>
         `
     };
-};});
+};*/
 
-$(document).ready(function(){
-    let inputBusqueda=document.getElementById("inputBusqueda");
-    let botonBusqueda=document.getElementById("botonBusqueda");
-    botonBusqueda.addEventListener("click",()=>{
-    let terminoBusqueda=inputBusqueda.value;
-    console.log("El valor seleccionado es: " + terminoBusqueda);
-    fetch("https://digimon-api.vercel.app/api/digimon/name/" + terminoBusqueda)
+function postBoton() {
+    document.getElementById("principal").style.display="none";
+    let inputBusqueda = document.getElementById("inputBusqueda");
+    let botonBusqueda = document.getElementById("botonBusqueda");
+    //botonBusqueda.addEventListener("click", function (){
+        let terminoBusqueda = inputBusqueda.value;
+        console.log("El valor seleccionado es: "+terminoBusqueda);
+        fetch("https://digimon-api.vercel.app/api/digimon/name/"+terminoBusqueda)
+            .then(response => response.json())
+            .then(resp => {crearCard(resp)});
 
-    .then(response=>response.json())
-    .then(principal=>{borrarDiv(principal)})
-    .then(data=>{crearCard(data)});
-
-    function borrarDiv(){
-        let principal=document.getElementById("principal");
-        principal.removeChild();
-    };
-
-    function crearCard(data){
-        let contenedor=document.getElementById("contenedor");
-        contenedor.innerHTML ="";
-        contenedor.innerHTML +=
-      `
+        function crearCard(resp) {
+            let contenedor = document.getElementById("contenedor");
+            contenedor.innerHTML = "";
+            contenedor.innerHTML +=
+                `
       <div class="card" style="width: 20rem;">
     
-      <img src="${data.img}" class="card-img-top" id="imagen" alt="...">
+      <img src="${resp.img}" class="card-img-top" id="imagen" alt="...">
       <div class="card-body">
-        <h2 id="nombre" class="card-title">${data.name}</h2>
-        <p id="nivel" class="card-text">${data.level}</p>
+        <h2 id="nombre" class="card-title">${resp.name}</h2>
+        <p id="nivel" class="card-text">${resp.level}</p>
       </div>
       <div class="card-body">
         
-        <a href="https://digimon.fandom.com/wiki/${data.name}" target="_blank" class="btn btn-primary">Más información</a>
+        <a href="https://digimon.fandom.com/wiki/${resp.name}" target="_blank" class="btn btn-primary">Más información</a>
       </div>
       </div>
       </div>
       <br>`
-    };
-    });
-});
+        };
+    }//);
+//};
+
