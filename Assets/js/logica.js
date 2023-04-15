087e263be509b21b1c79176a3157881fae82f967
Claudio Santibanez <yeyecasb@hotmail.com>
2023-04-15 13:41:28 -0400

    Revision Profe

--- Assets/js/logica.js
+++ Assets/js/logica.js
@@ -1,4 +1,4 @@
-/*let contenido = document.getElementById("listado");
+let contenido = document.getElementById("listado");
 
 fetch('https://digimon-api.vercel.app/api/digimon')
     .then(response => response.json())
@@ -14,24 +14,26 @@ function crearListado(resp) {
         <td><strong>${datoTemporal.level}</strong></td></tr>
         `
     };
-};*/
+};
 
 function postBoton() {
-    document.getElementById("principal").style.display="none";
+    document.getElementById("principal").style.display = "none";
     let inputBusqueda = document.getElementById("inputBusqueda");
     let botonBusqueda = document.getElementById("botonBusqueda");
     //botonBusqueda.addEventListener("click", function (){
-        let terminoBusqueda = inputBusqueda.value;
-        console.log("El valor seleccionado es: "+terminoBusqueda);
-        fetch("https://digimon-api.vercel.app/api/digimon/name/"+terminoBusqueda)
-            .then(response => response.json())
-            .then(resp => {crearCard(resp)});
-
-        function crearCard(resp) {
-            let contenedor = document.getElementById("contenedor");
-            contenedor.innerHTML = "";
-            contenedor.innerHTML +=
-                `
+    let terminoBusqueda = inputBusqueda.value;
+    url = "https://digimon-api.vercel.app/api/digimon/name/" + terminoBusqueda;
+    console.log(url);
+    fetch("https://digimon-api.vercel.app/api/digimon/name/" + terminoBusqueda)
+        .then(response => response.json())
+        .then(resp => { crearCard(resp) });
+};
+function crearCard(resp) {
+    alert("Funciona");
+    let contenedor = document.getElementById("contenedor");
+    contenedor.innerHTML = "";
+    contenedor.innerHTML +=
+        `
       <div class="card" style="width: 20rem;">
     
       <img src="${resp.img}" class="card-img-top" id="imagen" alt="...">
@@ -40,13 +42,12 @@ function postBoton() {
         <p id="nivel" class="card-text">${resp.level}</p>
       </div>
       <div class="card-body">
-        
-        <a href="https://digimon.fandom.com/wiki/${resp.name}" target="_blank" class="btn btn-primary">Más información</a>
+         <a href="https://digimon.fandom.com/wiki/${resp.name}" target="_blank" class="btn btn-primary">Más información</a>
       </div>
       </div>
       </div>
       <br>`
-        };
-    }//);
+};
+    //);
 //};
 
