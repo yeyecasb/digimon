c2dd5b3719cef360735db2eb94a093c657f2aeb8
Claudio Santibanez <yeyecasb@hotmail.com>
2023-04-18 19:11:21 -0400

    Aún no

--- Assets/js/logica.js
+++ Assets/js/logica.js
@@ -1,14 +1,14 @@
-$(document).ready();
-let contenido = document.getElementById("listado");
+$(document).ready(function () {
+    fetch('https://digimon-api.vercel.app/api/digimon')
+        .then(response => response.json())
+        .then(resp => { crearListado(resp) });
 
-fetch('https://digimon-api.vercel.app/api/digimon')
-    .then(response => response.json())
-    .then(resp => { crearListado(resp) });
-
-function crearListado(resp) {
-    for (let datoTemporal of resp) {
-        contenido.innerHTML +=
-        `<article class="col-12 col-sm-6 col-md-4 col-lg-2 my-2">
+    function crearListado(resp) {
+        let contenido = document.getElementById("listado");
+        console.log(resp);
+        for (let datoTemporal of resp) {
+            contenido.innerHTML +=
+                `<article class="col-12 col-sm-6 col-md-4 col-lg-2 my-2">
             <div class="card">
                 <img class="img-fluid" src="${datoTemporal.img}" height="40" weight="40" id="imagen" alt="Card image cap">
                 <div class="card-body">
@@ -19,24 +19,27 @@ function crearListado(resp) {
             </div>
         </article>
         `
+        };
     };
-};
+});
 
 function postBoton() {
-    document.getElementById("principal").style.display = "none";
+    //document.getElementById("principal").style.display = "none";
     let inputBusqueda = document.getElementById("inputBusqueda");
     let terminoBusqueda = inputBusqueda.value;
-    fetch("https://digimon-api.vercel.app/api/digimon/name/"+terminoBusqueda)
+    fetch("https://digimon-api.vercel.app/api/digimon/name/" + terminoBusqueda)
         .then(response => response.json())
-        .then(dato => { crearCard(dato) });
+        .then(dato => { 
+            crearTarjeta(dato) 
+        });
 };
 
-let tarjeta = document.getElementById("tarjeta");
-
-function crearCard(dato) {
-    tarjeta.innerHTML = "";
-    tarjeta.innerHTML +=
-    `
+function crearTarjeta(dato) {
+    let principal = document.getElementById("principal");
+    console.log(dato);
+    principal.innerHTML = "";
+    principal.innerHTML +=
+        `
     <div class="card row justify-content-md-center text-center col-12 my-5" >
         <img src="${dato.img}" class="img-fluid" height="100" id="imagen" alt="...">
         <div class="card-body col-12 my-5">
