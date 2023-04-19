1257b57984717d930ae9d7ea3d05f7481bd9fed2
Claudio Santibanez <yeyecasb@hotmail.com>
2023-04-19 00:05:30 -0400

    Codigo listo

--- Assets/js/logica.js
+++ Assets/js/logica.js
@@ -2,13 +2,14 @@ $(document).ready(function () {
     fetch('https://digimon-api.vercel.app/api/digimon')
         .then(response => response.json())
         .then(resp => { crearListado(resp) });
+});
 
-    function crearListado(resp) {
-        let contenido = document.getElementById("listado");
-        console.log(resp);
-        for (let datoTemporal of resp) {
-            contenido.innerHTML +=
-                `<article class="col-12 col-sm-6 col-md-4 col-lg-2 my-2">
+function crearListado(resp) {
+    let contenido = document.getElementById("listado");
+    console.log(resp);
+    for (let datoTemporal of resp) {
+        contenido.innerHTML +=
+            `<article class="col-12 col-sm-6 col-md-4 col-lg-2 my-2">
             <div class="card">
                 <img class="img-fluid" src="${datoTemporal.img}" height="40" weight="40" id="imagen" alt="Card image cap">
                 <div class="card-body">
@@ -17,21 +18,17 @@ $(document).ready(function () {
                     <a href="https://digimon.fandom.com/wiki/${datoTemporal.name}" class="btn btn-dark">Ver Wiki</a>
                 </div>
             </div>
-        </article>
-        `
-        };
+        </article>`
     };
-});
+};
 
 function postBoton() {
-    //document.getElementById("principal").style.display = "none";
     let inputBusqueda = document.getElementById("inputBusqueda");
     let terminoBusqueda = inputBusqueda.value;
-    fetch("https://digimon-api.vercel.app/api/digimon/name/" + terminoBusqueda)
+    let url = "https://digimon-api.vercel.app/api/digimon/name/" + terminoBusqueda
+    fetch(url)
         .then(response => response.json())
-        .then(dato => { 
-            crearTarjeta(dato) 
-        });
+        .then(dato => { crearTarjeta(dato) });
 };
 
 function crearTarjeta(dato) {
@@ -39,16 +36,14 @@ function crearTarjeta(dato) {
     console.log(dato);
     principal.innerHTML = "";
     principal.innerHTML +=
-        `
-    <div class="card row justify-content-md-center text-center col-12 my-5" >
-        <img src="${dato.img}" class="img-fluid" height="100" id="imagen" alt="...">
-        <div class="card-body col-12 my-5">
-          <h2 id="nombre" class="card-title">${dato.name}</h2>
-          <p id="nivel" class="card-text">${dato.level}</p>
-        </div>
-        <div class="card-body">
-            <a href="https://digimon.fandom.com/wiki/${dato.name}" class="btn btn-dark">Ver Wiki</a>
-        </div>
-    </div>
-    `
+        `<div class=" container card d-flex justify-content-center col-12 col-md-6 my-3" >
+            <img src="${dato[0].img}" class="rounded mx-auto d-block" height="" id="imagen" alt="...">
+            <div class="card-body">
+                <h2 id="nombre" class="card-title text-center">${dato[0].name}</h2>
+                <p id="nivel" class="card-text text-center">${dato[0].level}</p>
+            </div>
+            <div class="card-body text-center">
+                <a href="https://digimon.fandom.com/wiki/${dato[0].name}" class="btn btn-dark">Ver Wiki</a>
+            </div>
+        </div>`
 };
